48c94d081e01604b8fc8fdbbf3b71fdf1f60318b
sheik-mostafizur <sheikmostafizur@gmail.com>
2024-02-06 19:26:04 +0600

    I think this is okay

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -37,19 +37,36 @@ $(document).ready(function () {
   $(".emrdNews").easyTicker();
 });
 
+// Responsive for chart
 $(document).ready(function () {
   function resizeSVG() {
     var $container = $(".home-emrd-dashboard .container"),
       width = $container.width(),
       height = $container.height(),
       scale = Math.min(width / 400, height / 400); // Assuming the original SVG size is 200x200
-    console.log(scale);
     $(
       "#ringMainContainer:not(.ringDataShow) .emrd-ring-container .sdg-wheel-wrap .c-ring"
     ).css("transform", "scale(" + scale + ")");
   }
 
-  // Call resizeSVG on window resize and initial load
-  $(window).resize(resizeSVG);
-  resizeSVG();
+  function rightDataPrint() {
+    const isRingDataShow = $("#ringMainContainer").hasClass("ringDataShow");
+    if (!isRingDataShow) {
+      // Call resizeSVG on window resize and initial load
+      $(window).resize(resizeSVG);
+      resizeSVG();
+    } else {
+      $("#emrdRing").css("transform", "");
+    }
+  }
+
+  //   checking ringDataShow
+  $(window).on("load", function () {
+    rightDataPrint();
+    $(
+      "#ringMainContainer:not(.ringDataShow) .emrd-ring-container .sdg-wheel-wrap .c-ring"
+    ).on("click", function () {
+      rightDataPrint();
+    });
+  });
 });
