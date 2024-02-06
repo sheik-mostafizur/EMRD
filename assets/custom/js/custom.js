4cfe63a2cedfaed4db636a0063ead46a73eadd99
sheik-mostafizur <sheikmostafizur@gmail.com>
2024-02-06 18:15:48 +0600

    trying to fix chart size using js

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -1,42 +1,55 @@
-
 /**
  * Top Searchbar
  */
-(function ($){
-    "use strict";
+(function ($) {
+  "use strict";
 
-    function sdgTopSearch(){
-        var sdgSrcBtnIcon = $(".topBtnSrc");
-        var sdgSrcContentainer = $("#searchOuter");
-        var sdgSrcContentClose = $(".sdgSrcClose");
+  function sdgTopSearch() {
+    var sdgSrcBtnIcon = $(".topBtnSrc");
+    var sdgSrcContentainer = $("#searchOuter");
+    var sdgSrcContentClose = $(".sdgSrcClose");
 
-        sdgSrcBtnIcon.on('click', function () {
-            sdgSrcContentainer.toggleClass('sdgSrcOpen');
-            $('body').toggleClass('scrollOff');
-            console.log('Clicked Src')
-        });
+    sdgSrcBtnIcon.on("click", function () {
+      sdgSrcContentainer.toggleClass("sdgSrcOpen");
+      $("body").toggleClass("scrollOff");
+      console.log("Clicked Src");
+    });
 
-        sdgSrcContentClose.on('click', function () {
-            sdgSrcContentainer.toggleClass('sdgSrcOpen');
-            $('body').toggleClass('scrollOff');
-        });
+    sdgSrcContentClose.on("click", function () {
+      sdgSrcContentainer.toggleClass("sdgSrcOpen");
+      $("body").toggleClass("scrollOff");
+    });
 
-        $(document).keyup(function (e) {
-			if ( sdgSrcContentainer.hasClass('sdgSrcOpen') && e.keyCode === 27 ) {
-				sdgSrcContentainer.removeClass('sdgSrcOpen');
-				$('body').removeClass('scrollOff');
-			}
-		});
-    }
-    sdgTopSearch();
-
+    $(document).keyup(function (e) {
+      if (sdgSrcContentainer.hasClass("sdgSrcOpen") && e.keyCode === 27) {
+        sdgSrcContentainer.removeClass("sdgSrcOpen");
+        $("body").removeClass("scrollOff");
+      }
+    });
+  }
+  sdgTopSearch();
 })(jQuery);
 
 /**
  * Start Document Ready Function
  */
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".emrdNews").easyTicker();
+});
 
-    $('.emrdNews').easyTicker();
+$(document).ready(function () {
+  function resizeSVG() {
+    var $container = $(".home-emrd-dashboard .container"),
+      width = $container.width(),
+      height = $container.height(),
+      scale = Math.min(width / 400, height / 400); // Assuming the original SVG size is 200x200
+    console.log(scale);
+    $(
+      "#ringMainContainer:not(.ringDataShow) .emrd-ring-container .sdg-wheel-wrap .c-ring"
+    ).css("transform", "scale(" + scale + ")");
+  }
 
+  // Call resizeSVG on window resize and initial load
+  $(window).resize(resizeSVG);
+  resizeSVG();
 });
